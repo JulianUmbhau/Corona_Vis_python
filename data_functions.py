1a5bd435d6e9670c3f7a55dece6d9a9362cb8c14
Julian Johannes Umbhau Jensen <jensen_303@hotmail.com>
2021-10-23 22:24:37 +0200

    testing dash

--- data_functions.py
+++ data_functions.py
@@ -120,6 +120,14 @@ confirmed_deaths_clean = create_delta_values(confirmed_deaths_clean_population)
 # %%
 
 import dash
+import dash_leaflet as dl
+
+app = dash.Dash()
+app.layout = dl.Map(dl.TileLayer(), style={'width': '1000px', 'height': '500px'})
+
+if __name__ == '__main__':
+    app.run_server() 
+
 
 
 
